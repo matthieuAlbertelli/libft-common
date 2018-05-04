5cfc4f271e7b8d6b8c35ee506d0d34ef6c2c87cd
malberte <matthieu.albertelli@gmail.com>
2018-05-04 11:40:52 +0200

    correction norminette

--- ft_lstfind.c
+++ ft_lstfind.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstfind.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acoulomb <acoulomb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: malberte <malberte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/02 16:54:15 by malberte          #+#    #+#             */
-/*   Updated: 2018/05/02 18:07:57 by acoulomb         ###   ########.fr       */
+/*   Updated: 2018/05/04 11:37:36 by malberte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list			*ft_lstfind(const t_list *lst, void *data, int (*f_cmp)(const void *d1, const void *d2))
+t_list			*ft_lstfind(const t_list *lst, void *data,
+							int (*f_cmp)(const void *d1, const void *d2))
 {
 	t_list *iter;
 
--- ft_lstremove.c
+++ ft_lstremove.c
@@ -6,17 +6,18 @@
 /*   By: malberte <malberte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/02 18:56:55 by malberte          #+#    #+#             */
-/*   Updated: 2018/05/02 19:13:39 by malberte         ###   ########.fr       */
+/*   Updated: 2018/05/04 11:37:23 by malberte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstremove(t_list **lst, void *data, int (*f_cmp)(const void *d1, const void *d2),
-												void (*del)(void *, size_t))
+t_list	*ft_lstremove(t_list **lst, void *data,
+						int (*f_cmp)(const void *d1, const void *d2),
+						void (*del)(void *, size_t))
 {
 	t_list *to_delete;
-	
+
 	while (*lst && (*f_cmp)((*lst)->content, data) != 0)
 		lst = &((*lst)->next);
 	if (*lst)
